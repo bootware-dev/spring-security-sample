0f70475ad9c3693f72531fc3a6bf86ef817d0fa6
Jhon B Ware <dev@bootware.org>
2020-09-20 23:03:00 +0900

    Initial commit

---
+++ src/test/java/org/bootware/sample/springsec/SpringSecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bootware.sample.springsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
